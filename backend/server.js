8c22a8c060f0bdc99bd851a003e7c213a696adcd
Ying <ohis.yingshiuan@gmail.com>
2026-03-09 18:14:34 +0100

    typo: /

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }))
 
 
 const allowedOrigins = process.env.NODE_ENV === 'production'
-  ? ['https://menugen.insdash.ch', 'https://menu-gen-dusky.vercel.app/']
+  ? ['https://menu-gen-dusky.vercel.app', 'https://menugen.insdash.ch' ]
   : ['http://192.168.1.163:5173'];
 
 app.use(cors({
