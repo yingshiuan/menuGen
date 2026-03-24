b0d6bbe84eae59e2c02e51ededaf55498f28d8ca
Ying <ohis.yingshiuan@gmail.com>
2026-03-24 16:17:23 +0100

    fix: banner

--- frontend/src/components/TopBanner.vue
+++ frontend/src/components/TopBanner.vue
@@ -4,10 +4,11 @@ import { ref } from 'vue'
 const props = defineProps({
   message: {
     type: String,
-    default: 'This app uses a free backend hosting service, so the first request may take a few seconds while the server wakes up.'
+    default: 'First request may take a few seconds (free tier)'
   }
 })
 // If the first export doesn’t work the first time, don’t be afraid to try again — it’s normal!
+//This app uses a free backend hosting service, so the first request may take a few seconds while the server wakes up.
 
 const visible = ref(true)
 
@@ -18,7 +19,7 @@ const close = () => {
 
 
 <template>
-  <div v-if="visible" class="banner flex px-4 py-1 text-xs text-white bg-blue-500 item-center justify-between">
+  <div v-if="visible" class="fixed bottom-0 left-0 w-full flex px-4 py-1 text-xs text-white bg-blue-500 item-center justify-between">
     <span>{{ props.message }}</span>
     <button class="text-white cursor-pointer font-bold hover:text-blue-700" @click="close">✕</button>
   </div>
